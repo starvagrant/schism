ca2ac71302f42b3859538c6194e9e3281cf15934
James Ginns <starvagrant@yahoo.com>
2022-06-22 13:41:25 -0500

    Add ability to execute shell commands from cli

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import cmd
+import subprocess
 
 class TestCmd(cmd.Cmd):
     prompt = "test> "
@@ -13,6 +14,15 @@ class TestCmd(cmd.Cmd):
 
     def do_quit(self,args):
         return True
+
+    def do_bash(self, args):
+        arg_list = args.split(" ")
+        print(arg_list)
+        process = subprocess.run(arg_list, universal_newlines=True, stdout=subprocess.PIPE)
+        print(process.stdout)
+        #process = subprocess.run(args, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #print(process.stdout)
+        #print(process.stderr)
 
 if __name__ == '__main__':
     cli = TestCmd()
