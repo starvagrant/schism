3323046cdbf4c25e80eff9291ec85e4892739c10
James Ginns <starvagrant@yahoo.com>
2022-08-10 14:13:08 -0500

    Transition from subprocess.run to subprocess.Popen
    Move to subprocess.Popen in order to be able to send signals
    to running instances of play, rather than being forced to abort
    the program with Control-C.

--- taskcli.py
+++ taskcli.py
@@ -3,6 +3,8 @@
 import cmd
 import subprocess
 import yaml
+import os
+import signal
 
 DANGER = "\033[31m"     #red
 CAUTION = "\033[33m"    #yellow
@@ -18,6 +20,8 @@ class TestCmd(cmd.Cmd):
     def __init__(self):
         super().__init__()
         self.yaml = []
+        self.play_process = None
+        self.play_pid = -1
 
     def precmd(self, line):
         with open('/home/jginns/.schism/taskcli_history', 'a') as f:
@@ -73,44 +77,45 @@ class TestCmd(cmd.Cmd):
 
     def do_play(self, args):
         """
+        play <name> <times>
         Play a helpful track repeatedly.
         These Include:
         game: a mash of Nintendo era soundtracks
         venus: Gustav Holst's Venus Bringer of Peace
         wakeup: FF7, Cloud's Motorcycle Sequence
         clean: Warcraft II soundtrack, tracks 2-13
+        zero: almost null mp3 (stops current track)
         """
 
-
+        if self.play_process is not None:
+            self.play_process.kill()
         arg = args.split(' ')
         print(arg)
         run_list = arg[0]
         if len(arg) < 2:
-            arg.append(0)
+            arg.append(1)
             print(arg)
         else:
             print(arg)
-        if int(arg[1]) == 0:
-            while True:
-                song = '~/Music/Bash/' + arg[0] + '.mp3'
-                process=subprocess.run(['play', song])
-
         for x in range(int(arg[1])):
             if arg[0] == 'relax':
-                process=subprocess.run(['play', '~/Music/Bash/relax.mp3'], universal_newlines=True)
+                self.play_process=subprocess.Popen(['play', '~/Music/Bash/relax.mp3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 print('relax!!')
             if arg[0] == 'game':
-                process=subprocess.run(['play', '~/Music/Bash/BlasterMaster7.ogg', '~/Music/Bash/SuperMario3BowserBattle.mp3', '~/Music/Bash/WilyFortress.mp3'])
+                self.play_process=subprocess.Popen(['play', '~/Music/Bash/BlasterMaster7.ogg', '~/Music/Bash/SuperMario3BowserBattle.mp3', '~/Music/Bash/WilyFortress.mp3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 print('Blaster SM3 Wily')
             if arg[0] == 'venus':
-                process=subprocess.run(['play', '~/Music/Bash/venus.mp3'])
+                self.play_process = subprocess.Popen(['play', '~/Music/Bash/venus.mp3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 print('Venus')
             if arg[0] == 'wakeup':
-                process=subprocess.run(['play', '~/Music/Bash/wakeup.mp3'])
+                self.play_process = subprocess.Popen(['play', '~/Music/Bash/wakeup.mp3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 print('wakuep')
             if arg[0] == 'clean':
-                process=subprocess.run(['play', '~/Music/Bash/Warcraft\\ II/*.ogg'])
+                self.play_process=subprocess.Popen(['play', '~/Music/Bash/Warcraft\\ II/*.ogg'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 print('clean')
+            if arg[0] == 'zero':
+                self.play_process=subprocess.Popen(['play', '~/Music/Bash/zero.mp3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                print('zero')
 
     def do_display(self,args):
         for todos in self.yaml['Undone'].keys():
