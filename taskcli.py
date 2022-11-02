f87a67f457bfc7b751fb7b0a584d79b71737939d
James Ginns <starvagrant@yahoo.com>
2022-11-02 14:45:30 -0500

    Implement defer with undone list
    Remove the previous method of handling deferred actions, which
    was sending them to a separate deferred list. As the queue command
    is the main task reminder, simply making a task disappear from
    queue is enough to defer it.

--- taskcli.py
+++ taskcli.py
@@ -113,9 +113,7 @@ class TestCmd(cmd.Cmd):
             pos = int(args.split()[1])
             try:
                task = self.yaml['Undone'][key].pop(pos)
-               if None in self.yaml['Defer'][key]:
-                   self.yaml['Defer'][key].pop(0)
-               self.yaml['Defer'][key].append(task)
+               self.yaml['Undone'][key].append(task)
                hist_file = os.path.expanduser('~/.schism/history/taskdefer_history')
                with open(hist_file, 'a') as f:
                    timestamp=str(int(time.time()))
